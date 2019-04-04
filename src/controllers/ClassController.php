fc669d928f2fa4c9c043578a33e379bf73416729
Edoardo Genova <genova.edoardo@gmail.com>
2019-04-04 17:58:59 +0200

    Added getClasses function in ClassController

--- src/controllers/ClassController.php
+++ src/controllers/ClassController.php
@@ -8,7 +8,20 @@ class ClassController {
 
     //GET /admin/classe
     static function getClasses($req, $res, $service, $app){
-		$res->json([]);
+        $stm = $app->db->prepare('SELECT id_classe, nome, anno, abilita FROM classe');
+        $stm->execute();
+        $dbres = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+        $data = array_map(function($entry){
+            return [
+                'id_classe' => +$entry['id_classe'],
+                'nome' => $entry['nome'],
+                'anno_scolastico' => $entry['anno'],
+                'enabled' => $entry['abilita'],
+            ];
+        }, $dbres);
+
+        $res->json($data);
     }
 
     // POST /admin/classe
