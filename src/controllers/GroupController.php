b7c0e31f30f2abfacea1b2b08a1ddc05fd0a5fcf
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-04-30 22:17:01 +0200

    Added JoinUser method

--- src/controllers/GroupController.php
+++ src/controllers/GroupController.php
@@ -8,8 +8,19 @@ class GroupController {
 
     // POST /user/gruppo
     static function joinGroup($req, $res, $service, $app){
-		
-        $res->json([]);
+		    $parameters = $req->body();
+            $parameters = json_decode($parameters, true);
+            $stm = $app->db->prepare('UPDATE studente SET id_gruppo=:id_gruppo WHERE id_studente = :id_studente');
+            $stm->bindValue(":id_gruppo", $parameters['id_gruppo']);
+            $stm->bindValue(":id_studente", $parameters['id_studente']);
+            $stm->execute();
+    		if($stm->rowCount() > 0)
+    		{
+    			$res->json(["message" => "OK", "code" => 200 ]);
+    		}
+    		else{
+    			$res->json(["message" => "Studente non aggiunto al gruppo", "code" => 500 ]);
+    		}
     }
 
     // DELETE /user/gruppo
@@ -25,10 +36,10 @@ class GroupController {
             $res->json(["message" => "Gruppo non lasciato", "code" => 500 ]);
         }
     }
-	
+
 	// GET /user/gruppo
     static function getGroupMembers($req, $res, $service, $app){
-		
+
         $res->json([]);
     }
 
