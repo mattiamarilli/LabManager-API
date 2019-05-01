88f4f8af382342b301d1c47d1ba7c16ecd77e0d5
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-05-01 17:10:08 +0200

    Fixed class name in AuthController

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -14,19 +14,20 @@ class AuthController {
     $stm->execute();
     if($stm->rowCount() > 0){
       $dbres = $stm->fetchAll(PDO::FETCH_ASSOC);
-      $stm = $app->db->prepare('SELECT nome FROM classe where id_classe=:id_classe AND abilita=true');
-      $stm->bindValue(":id_classe", $parameters['id_classe']);
+      $stm = $app->db->prepare('SELECT nome as nomeclasse FROM classe where id_classe=:id_classe AND abilita=true');
+      $stm->bindValue(":id_classe", $dbres[0]['id_classe']);
       $stm->execute();
       if($stm->rowCount() > 0){
-        $dbres2 = $stm->fetchAll(PDO::FETCH_ASSOC);
-        $data = array_map(function($entry, $dbres2){
+        $var = $stm->fetchAll(PDO::FETCH_ASSOC);
+        $dbres[0]['nome_classe'] = $var[0]['nomeclasse'];
+        $data = array_map(function($entry){
             return [
                 'id' => +$entry['id_studente'],
                 'nome' => $entry['nome'],
                 'cognome' => $entry['cognome'],
                 'id_gruppo' => $entry['id_gruppo'] ? +$entry['id_gruppo'] : null,
                 'id_classe' => +$entry['id_classe'],
-                'classe' => $dbres2['nome']
+                'classe' => $entry['nome_classe']
             ];
         }, $dbres);
         $res->json($data);
