9cb2da86eb91254e5b24aba037680106554fe9b4
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-05-09 22:46:14 +0200

    Fix

--- src/controllers/StatsController.php
+++ src/controllers/StatsController.php
@@ -1,7 +1,7 @@
 <?php
 class StatsController{
   static function deletedTool($req, $res, $service, $app){
-    $stm = $app->db->prepare('SELECT u.nome, c.categoria as categoria from utensile u INNER JOIN categoria c ON u.id_categoria=c.id_categoria WHERE deleted=true');
+    $stm = $app->db->prepare('SELECT u.nome, c.nome as categoria from utensile u INNER JOIN categoria c ON u.id_categoria=c.id_categoria WHERE deleted=true');
     $stm->execute();
     $dbres = $stm->fetchAll(PDO::FETCH_ASSOC);
     $data = array_map(function($entry){
