a31232544fc2f08205f448590071dbbffe94e028
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-05-18 14:35:58 +0200

    Stats fix

--- src/controllers/StatsController.php
+++ src/controllers/StatsController.php
@@ -1,11 +1,13 @@
 <?php
 class StatsController{
   static function deletedTool($req, $res, $service, $app){
-    $stm = $app->db->prepare('SELECT u.nome, c.nome as categoria from utensile u INNER JOIN categoria c ON u.id_categoria=c.id_categoria WHERE deleted=true');
+    $stm = $app->db->prepare('SELECT u.id_utensile, u.id_categoria, u.nome, c.nome as categoria from utensile u INNER JOIN categoria c ON u.id_categoria=c.id_categoria WHERE deleted=true');
     $stm->execute();
     $dbres = $stm->fetchAll(PDO::FETCH_ASSOC);
     $data = array_map(function($entry){
       return [
+        'id_utensile' => +$entry['id_utensile'],
+        'id_categoria' => +$entry['id_categoria'],
         'nome' => $entry['nome'],
         'categoria' => $entry['categoria'],
       ];
