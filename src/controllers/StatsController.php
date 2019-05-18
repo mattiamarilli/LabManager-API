80c71d3720aeb734d796e50a2b5f25ebc7da6cf3
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-05-18 16:56:04 +0200

    Stats fix

--- src/controllers/StatsController.php
+++ src/controllers/StatsController.php
@@ -50,7 +50,7 @@ class StatsController{
     //$paramaters = $req->headers();
     $paramaters = $req->body();
     $paramaters = json_decode($paramaters, true);
-    $stm = $app->db->prepare('SELECT evento.id_utensile, classe.nome AS classe, COUNT(studente.nome) AS utilizzi, MIN(evento.inizio) AS inizio, MAX(evento.fine) AS fine FROM studente_evento INNER JOIN studente ON studente_evento.id_studente=studente.id_studente INNER JOIN classe ON classe.id_classe=studente.id_classe INNER JOIN evento ON evento.id_evento=studente_evento.id_evento GROUP BY evento.id_utensile, classe.nome');
+    $stm = $app->db->prepare('SELECT evento.id_utensile, classe.nome AS classe, COUNT(evento.id_utensile) AS utilizzi, MIN(evento.inizio) AS inizio, MAX(evento.fine) AS fine FROM studente_evento INNER JOIN studente ON studente_evento.id_studente=studente.id_studente INNER JOIN classe ON classe.id_classe=studente.id_classe INNER JOIN evento ON evento.id_evento=studente_evento.id_evento GROUP BY evento.id_utensile, classe.nome');
     if($stm->execute()){
       $dbres = $stm->fetchAll(PDO::FETCH_ASSOC);
       $data = array_map(function($entry){
