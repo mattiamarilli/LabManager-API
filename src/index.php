10ff1994b920050ffd90081b8681d82593e3094b
2000AndreaPaolo <andrea.andre60@gmail.com>
2019-05-21 10:33:31 +0200

    CORS

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, token');
 header('Access-Control-Allow-Credentials: true');
 
 foreach (glob("controllers/*Controller.php") as $filename)
